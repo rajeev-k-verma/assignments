2cf69e08897743604ba81364ed1278071e8bee17
verma-rajeev <rajeevverma.ald@gmail.com>
2023-12-16 11:54:54 +0530

    Completed expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,43 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let ans = [];
+  
+  transactions.forEach(transaction => {
+    let flag = false;
+    ans.forEach(i => {
+      if(i.category == transaction.category){
+        i.totalSpent += transaction.price;
+        flag = true;
+      }
+    });
+    if(!flag)
+      ans.push(newObject(transaction.category, transaction.price));
+    
+  });
+  return ans;
 }
+
+function newObject(categoryName, price){
+  return {
+    category: categoryName,
+    totalSpent: price
+  }
+}
+
+// function calculateTotalSpentByCategory(transactions) {
+//   let ans = new Map();
+
+//   transactions.forEach(transaction => {
+//     if (ans.has(transaction.category))
+//       ans.set(transaction.category, ans.get(transaction.category) + transaction.price);
+//     else
+//       ans.set(transaction.category, transaction.price);
+//   });
+
+//   return Array.from(ans, ([category, price]) => ({category: category, totalSpent: price}));
+// }
 
 module.exports = calculateTotalSpentByCategory;
