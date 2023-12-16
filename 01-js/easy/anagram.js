9f7b25435ed632c6a19a73299fc9de3f15464cbc
verma-rajeev <rajeevverma.ald@gmail.com>
2023-12-16 10:59:59 +0530

    Completed anagram question

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !== str2.length) return false;
 
+  let str2Lower = str2.toLowerCase();
+  let str1Lower = str1.toLowerCase();
+
+  for(let i = 0; i < str1.length; i++) {
+    //check if the character is present in str2 without case sensitivity
+    if(str2Lower.indexOf(str1Lower[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
