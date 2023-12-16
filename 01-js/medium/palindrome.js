612054813b0a473c5a4ce4e3688a10afce39da13
verma-rajeev <rajeevverma.ald@gmail.com>
2023-12-16 14:40:12 +0530

    Completed palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,12 @@
 */
 
 function isPalindrome(str) {
+  //use regular expressions
+  str = str.replace(/[^A-Za-z]/g, '').toLowerCase();
+  let n = str.length;
+  for(let i = 0; i <= n / 2; i++)
+    if(str.charAt(i) != str.charAt(n-1 - i))
+      return false;
   return true;
 }
 
